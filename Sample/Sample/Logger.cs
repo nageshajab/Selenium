4479a8d56a48e7a42bf01fc5ca2a4d89ce13d03a
Nagesh Ajab <nagesh.ajab@synerzip.com>
2021-06-06 12:52:56 +0530

    .gitignore is now working

---
+++ Sample/Sample/Logger.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using System;
+
+namespace SmokeTests
+{
+    public class Logger
+    {
+        private static readonly NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Error(string step, string status, string msg, string exception = "")
+        {
+            string logmsg = $"StepName: {step}, Status: {status}, Date: { DateTime.Now:MM/dd/yyyy HH:mm}, Exception: {exception}, Msg: {msg}";
+            Console.WriteLine(logmsg);
+            logger.Error(logmsg);
+        }
+        public static void Info(string step, string status, string msg)
+        {
+            string logmsg = $"StepName: {step}, Status: {status}, Date: { DateTime.Now:MM/dd/yyyy HH:mm}, Exception: NONE, Msg: {msg}";
+            Console.WriteLine(logmsg);
+            logger.Info(logmsg);
+        }
+    }
+}
---
+++ Sample/Sample/Selenium.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SmokeTests
+{
+    public class Selenium
+    {
+        static WebDriverWait wait = null;
+        static IWebDriver driver = null;
+        static Actions action = null;
+
+        public static void LaunchUrl(string url)
+        {
+            driver = new ChromeDriver(".");
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl(url);
+            WebDriverwait(120, 5);
+        }
+
+        public static void Quit()
+        {
+            driver.Quit();
+        }
+
+        public static void WaitUntilElementIsVisibleByXpath(string xpath)
+        {
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(xpath)));
+        }
+
+        public static void WaitUntilElementIsVisibleById(string id)
+        {
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(id)));
+        }
+
+        public static IWebElement FindElementByXpath(string xpath)
+        {
+            return driver.FindElement(By.XPath(xpath));
+        }
+
+        public static IWebElement FindElementById(string id)
+        {
+            return driver.FindElement(By.Id(id));
+        }
+
+        public static void WebDriverwait(int fromseconds, int pollingIntervalSeconds)
+        {
+            wait = new WebDriverWait(driver, timeout: TimeSpan.FromSeconds(fromseconds))
+            {
+                PollingInterval = TimeSpan.FromSeconds(pollingIntervalSeconds),
+            };
+        }
+        public static void MoveToElementByXpath(string xpath)
+        {
+            action = new Actions(driver);
+            action.MoveToElement(FindElementByXpath(xpath)).Perform();
+        }
+     
+    }
+}
